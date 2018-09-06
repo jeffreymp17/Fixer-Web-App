f7e08822174c5082d297e1fec5446a1f844eb4f0
jf2028mp <jeffry5800@gmail.com>
2018-09-05 18:32:13 -0600

    View technicians and components created

---
+++ src/app/app.router.ts
@@ -0,0 +1,11 @@
+import {Routes,RouterModule} from "@angular/router";
+import {ModuleWithProviders} from "@angular/core";
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import{TechniciansComponent} from './components/technicians/technicians.component';
+const appRoutes:Routes=[
+{path:'',redirectTo:'/home',pathMatch:'full'},
+{path:'home',component:HomeComponent},
+{path:"technicians",component:TechniciansComponent}
+];
+export const routes:ModuleWithProviders=RouterModule.forRoot(appRoutes);
