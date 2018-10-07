5dbb710498b6441f16e22e8856408cce3af223f1
Hedryan <hedryanrojas@gmail.com>
2018-10-06 19:14:30 -0600

    UI changes

--- src/app/models/response.model.ts
+++ src/app/models/response.model.ts
@@ -1,10 +1,13 @@
 import {Links} from './links.model'; 
 import{Meta} from './meta.model';
-export class ResponseModel{
-	data:any[];
-	links:Links;
-	meta:Meta;
-	constructor(){
 
+export class ResponseModel{
+	public data:any;
+	public links:Links;
+	public meta:Meta;
+	constructor(data:any,links:Links,meta:Meta){
+		this.data = data;
+		this.links = links;
+		this.meta = meta;
 	}
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -7,18 +7,22 @@ export class User{
 	public picture:string;
 	public phone:string;
 	public type:string;
+	public userable: any;
 	public created_at:Date;
 
 	constructor(id:number=-1, name:string="",
 		lastname:string="", picture:string="",
 		phone:string="", 
-		type:string="",created_at:Date =null){
+		type:string="",
+		userable:any = null,
+		created_at:Date =null){
 		this.id=id;
 		this.name=name;
 		this.lastname=lastname;
 		this.picture=picture;
 		this.phone=phone;
 		this.type = type;
+		this.userable = userable;
 		this.created_at=created_at;
 	}
 
